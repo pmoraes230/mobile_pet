92ab0afcdac0b5b5e19de2ce32209dc7890954e7
Rayan Rodrigues <rayan.estudo@outlook.com>
2026-05-26 17:11:20 -0300

    resolvi o bug do cupdi e de adoçao e tambem o do perfil

--- src/utils/endPoint_Url.js
+++ src/utils/endPoint_Url.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export const API_URL = "http://10.0.60.91:3000"
-=======
-export const API_URL = "http://10.0.60.40:3000"
->>>>>>> ff4d6cc5c00570da41ffd58b46b5cd8ff4512b87
 export const _API_URL_PROD = "https://api-pet-fmdo.onrender.com"
