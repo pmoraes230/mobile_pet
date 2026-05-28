6f2a85af24a1f34deedfffed3f597006751f06cd
Patrick Nascimento <pmoraes230nascimento@gmail.com>
2026-05-28 16:46:50 -0300

    mudança de endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import * as SecureStore from 'expo-secure-store';
 const TOKEN_KEY = "auth_token";
 
 const api = axios.create({
-  baseURL: 'http://10.0.60.216:3000/api',
+  baseURL: 'http://https://api-pet-fmdo.onrender.com/api',
 });
 
 api.interceptors.request.use(
