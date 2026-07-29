785058dd02119624ee327d0266b8c1f10be3e854
Rayan Rodrigues <rayan.estudo@outlook.com>
2026-07-29 15:06:40 -0300

    mesma coisa

--- src/screens/TelaInicial/TelaInicial.jsx
+++ src/screens/TelaInicial/TelaInicial.jsx
@@ -44,7 +44,7 @@ const formatarCompromisso = (compromisso, t, language) => {
   if (!compromisso) {
     return {
       titulo: t('Sem agendamentos'),
-      subtitulo: t('Você não possui agendamentos futuros.'),
+      subtitulo: t('Você não possui consultas marcadas no momento.'),
     };
   }
 
